f8ca0352de9e16a02795e4d007ef4912812c7b17
Gabor Cserto <csertog@gmail.com>
2020-06-29 16:01:27 +0200

    Working with CSS Moduless & Media Queries

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import styled from 'styled-components';
 
-const StyledDiv = styled.div`
-	border: 1px solid #eee;
-	box-shadow: 0 2px 3px #ccc;
-	margin: 16px auto;
-	padding: 16px;
-	text-align: center;
-	width: 60%;
-	
-	@media (min-width: 500px) {
-		width: 450px;
-	}
-`;
+import classes from './Person.module.css';
 
 const person = props => {
 	return (
-		<StyledDiv>
+		<div className={classes.Person}>
 			<p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
 			<p>{props.children}</p>
 			<input type="text" onChange={props.changed} defaultValue={props.name}/>
-		</StyledDiv>
+		</div>
 	)
 }
 
