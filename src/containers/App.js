ebe2e2b7503e76d9f40338c3ab7968331b07d697
Gabor Cserto <csertog@gmail.com>
2020-07-10 12:08:57 +0200

    Component Creation Lifecycle in Action

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,6 +5,11 @@ import Cockpit from '../components/Cockpit/Cockpit';
 
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        console.log('[App.js] constructor');
+    }
+
     state = {
         persons: [
             { id: 'adfasd', name: 'Max', age: 28 },
@@ -13,6 +18,20 @@ class App extends Component {
         ],
         otherState: 'Some other value',
         showPersons: false
+    }
+
+    static getDerivedStateFromProps(props, state) {
+        console.log('[App.js] getDerivedStateFromProps', props);
+        return state;
+    }
+
+    componentWillMount() {
+        //deprecated
+        console.log('[App.js] componentWillMount');
+    }
+
+    componentDidMount() {
+        console.log('[App.js] componentDidMount');
     }
 
     nameChangedHandler = (event, id) => {
@@ -45,18 +64,21 @@ class App extends Component {
     };
 
     render() {
+        console.log('[App.js] render');
         let persons = null;
 
         if (this.state.showPersons) {
-            persons = <Persons
-                        persons={this.state.persons}
-                        clicked={this.deletePersonsHandler}
-                        changed={this.nameChangedHandler} />;
+            persons =
+                <Persons
+                    persons={this.state.persons}
+                    clicked={this.deletePersonsHandler}
+                    changed={this.nameChangedHandler} />;
         }
 
         return (
             <div className={classes.App}>
                 <Cockpit
+                    title={this.props.appTitle}
                     showPersons={this.state.showPersons}
                     persons={this.state.persons}
                     clicked={this.togglePersonsHandler}
