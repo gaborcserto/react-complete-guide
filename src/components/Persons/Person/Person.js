58f75658c293bbf8230e057635ec86c3d5710c80
Gabor Cserto <csertog@gmail.com>
2020-07-17 16:23:31 +0200

    Rendering Adjacent JSX Elements par2

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -7,9 +7,9 @@ class Person extends Component {
 	render() {
 		console.log('[Person.js] rendering...');
 		return [
-			<p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>,
-			<p>{this.props.children}</p>,
-			<input type="text" onChange={this.props.changed} defaultValue={this.props.name}/>
+			<p key="i1" onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>,
+			<p key="i2">{this.props.children}</p>,
+			<input key="i3" type="text" onChange={this.props.changed} defaultValue={this.props.name}/>
 		]
 	}
 }
